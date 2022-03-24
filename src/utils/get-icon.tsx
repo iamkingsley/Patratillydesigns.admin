666273bf21f06f9fb27829760917c4674d3150b9
Derric <realbenneh@gmail.com>
2022-03-24 09:43:42 +0000

    added mp api key and changed socialIcon

--- src/utils/get-icon.tsx
+++ src/utils/get-icon.tsx
@@ -5,9 +5,10 @@ type Props = {
 };
 export const getIcon = ({ iconList, iconName, ...rest }: Props) => {
   const TagName = iconList[iconName];
-  return !!TagName ? (
-    <TagName {...rest} />
-  ) : (
-    <p className="text-sm text-red-500">{iconName} is not a valid icon</p>
-  );
+  // return !!TagName ? (
+  //   <TagName {...rest} />
+  // ) : (
+  //   <p className="text-sm text-red-500">{iconName} is not a valid icon</p>
+  // );
+  return !!TagName ?? <TagName {...rest} />
 };
