c218607d5089523810602d03b0e805777a8c2e09
Derric <realbenneh@gmail.com>
2022-04-26 16:02:38 +0000

    added icons

--- src/settings/site.settings.ts
+++ src/settings/site.settings.ts
@@ -119,12 +119,12 @@ export const siteSettings = {
       {
         href: ROUTES.FILE_MANAGER,
         label: "sidebar-nav-item-file-manager",
-        icon: "",
+        icon: "TypesIcon",
       },
       {
         href: ROUTES.GALLERY,
         label: "sidebar-nav-item-gallery",
-        icon: "",
+        icon: "WithdrawIcon",
       },
     ],
     shop: [
